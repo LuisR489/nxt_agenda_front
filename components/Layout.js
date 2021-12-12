69f206596be4afea779396ef6daba7fdd9eb980e
Paulo Tejada <devlagamers@gmail.com>
2021-12-12 18:46:06 -0500

    information

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({children}) {
     <div className={styles.layout}  >
       <div className={styles.layout__header} >
         <nav className={styles.layout__nav}>
-          <p className={styles.layout__title}>Information Soft</p>
+          <p className={styles.layout__title}>Information  Soft</p>
           <Link href='/dashboard' >
             <a className={styles.layout__link}>Inicio</a>
           </Link>
