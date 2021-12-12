bf130c7288bc88b689071b1def9bff18c6d64d6f
Paulo Tejada <devlagamers@gmail.com>
2021-12-12 11:51:28 -0500

    working on return

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,20 @@
-import React from 'react'
 import styles from '../styles/components/Layout.module.scss'
+import Link from "next/link"
 
 export default function Layout({children}) {
   return (
     <div className={styles.layout}  >
-      <nav className={styles.layout__nav} >
-        <p className={styles.layout__title}>Software</p>
-      </nav>
-      {children}
+      <div className={styles.layout__header} >
+        <nav className={styles.layout__nav}>
+          <p className={styles.layout__title}>Software</p>
+          <Link href='/dashboard' >
+            <a>Inicio</a>
+          </Link>
+        </nav>
+      </div>
+      <div className={styles.layout__container} >
+        {children}
+      </div>
     </div>
   )
 }
--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -9,106 +9,78 @@ import CallIcon from '@mui/icons-material/Call';
 
 export default function Dashboard({ registers }) {
 
-  const [calls, setCalls] = useState(['']);
+  const [calls, setCalls] = useState([]);
 
-  useEffect( ()=> console.log(registers))
+  useEffect( ()=> console.log(calls.length))
 
   return(
     <Layout>
       <div className={styles.dashboard} >
-        <h3 className={styles.dashboard__title} >Registro telefonico</h3>
-        <table className={styles.dashboard__table} >
-          <thead className={styles.dashboard__table_thead}>
-            <tr>
-              <th>Id Aleatorio</th>
-              <th>Imei</th>
-              <th>Fecha Registro</th>
-              <th>Contactos</th>
-              <th>Mensajes</th>
-              <th>Llamadas</th>
-            </tr>
-          </thead>
-          <tbody className={styles.dashboard__table_tbody} >
-            {registers.data.map((row, index) => (
-              <tr key={index} >
-                <td>{row.id}</td>
-                <td>{row.attributes.imei}</td>
-                <td>{row.attributes.createdAt}</td>
-                <td>
-                  <PermContactCalendarIcon></PermContactCalendarIcon>
-                </td>
-                <td>
-                  <EmailIcon></EmailIcon>
-                </td>
-                <td onClick={ () => setCalls(row.attributes.arraycalls)} >
-                  <CallIcon></CallIcon>
-                </td>
-              </tr>
+        
+        <div className={styles.dashboard__header}>
+          <h4 className={styles.dashboard__title}> Registro telefonico </h4>
+          <p className={styles.dashboard__return} onClick={ () => setCalls([])} >Regresar</p>
+        </div>
+        {
+          calls.length === 0 && (
+            <table className={styles.dashboard__table} >
+              <thead className={styles.dashboard__table_thead}>
+                <tr>
+                  <th>Id Aleatorio</th>
+                  <th>Imei</th>
+                  <th>Fecha Registro</th>
+                  <th>Contactos</th>
+                  <th>Mensajes</th>
+                  <th>Llamadas</th>
+                </tr>
+              </thead>
+              <tbody className={styles.dashboard__table_tbody} >
+                {registers.data.map((row, index) => (
+                  <tr key={index} >
+                    <td>{row.id}</td>
+                    <td>{row.attributes.imei}</td>
+                    <td>{row.attributes.createdAt}</td>
+                    <td>
+                      <PermContactCalendarIcon></PermContactCalendarIcon>
+                    </td>
+                    <td>
+                      <EmailIcon></EmailIcon>
+                    </td>
+                    <td onClick={ () => setCalls(row.attributes.arraycalls)} >
+                      <CallIcon></CallIcon>
+                    </td>
+                  </tr>
+    
+                ) )}
+    
+              </tbody>
+            </table>
+          )
+        }
+       
+        { calls.length !== 0 && (
+            <table className={styles.dashboard__table}>
+              <thead className={styles.dashboard__table_thead}>
+                <tr>
+                  <th>Duracion</th>
+                  <th>Fecha</th>
+                  <th>Numero</th>
+                </tr>
+              </thead>
+              <tbody className={styles.dashboard__table_tbody} >
+                {calls?.map((row, index) => (
+                  <tr key={index} >
+                    <td>{row.duracion}</td>
+                    <td>{row.fecha}</td>
+                    <td>{row.numero}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) 
 
-            ) )}
-
-          </tbody>
-        </table>
-
-
-        <table className={styles.dashboard__table}>
-          <thead className={styles.dashboard__table_thead}>
-            <tr>
-              <th>Duracion</th>
-              <th>fecha</th>
-              <th>numero</th>
-            </tr>
-          </thead>
-          <tbody>
-            {calls?.map((row, index) => (
-              <tr key={index} >
-                <td>{row.duracion}</td>
-                <td>{row.fecha}</td>
-                <td>{row.numero}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-
-        {/* <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell align="right">Id Aleatorio</TableCell>
-              <TableCell align="right">Imei</TableCell>
-              <TableCell align="right">Fecha Registro</TableCell>
-              <TableCell align="center">Contactos</TableCell>
-              <TableCell align="center">Mensajes</TableCell>
-              <TableCell align="center">Llamadas</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {registers.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.dynamicid}</TableCell>
-                <TableCell align="right">{row.imei}</TableCell>
-                <TableCell align="right">{row.createdAt}</TableCell>
-                <TableCell align="center">
-                  <PermContactCalendarIcon></PermContactCalendarIcon>
-                </TableCell>
-                <TableCell align="center">
-                  <MailOutlineIcon></MailOutlineIcon>
-                </TableCell>
-                <TableCell align="center">
-                  <CallIcon></CallIcon>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-        </TableContainer> */}
+        }
+       
       </div>
     </Layout>
   )
